756d98cb109827624cdd9a6f32fe6b3fe2817f70
tractatus-86 <tractatus@instance-1.us-central1-a.c.geospatialdataetlandanalysis.internal>
2021-06-10 06:35:11 +0000

    fixed typo

--- source-boulder-country-data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sbt "runMain com.datasource.DataSourcing src/main/resources/configs/sourced/Source-BoulderCountyParcels.yaml"
-sbt "runMain com.datasource.DataSourcing src/main/resources/configs/sourced/Source-ASR_PublicDataFiles.yaml"
